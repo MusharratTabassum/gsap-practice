4afc8a8eb2062d8f6462b48406d6a9dc0045b9e6
Musharrat Tabassum <musharrat.dcastalia@gmail.com>
2022-08-16 20:11:15 +0600

    slider is working

--- app.js
+++ app.js
@@ -4,11 +4,27 @@
 //     duration: 3,
 //     scrollTrigger: '.square'
 // })
-
+//
+// gsap.registerPlugin(ScrollTrigger);
+//
+// let sections = gsap.utils.toArray(".panel");
+// console.log(sections);
+//
+// gsap.to(sections, {
+//     xPercent: -100 * (sections.length - 1),
+//     ease: "none",
+//     scrollTrigger: {
+//         trigger: ".container",
+//         pin: true,
+//         scrub: 1,
+//         snap: 1 / (sections.length - 1),
+//         // base vertical scrolling on how wide the container is so it feels more natural.
+//         end: "+=3500",
+//     }
+// });
 gsap.registerPlugin(ScrollTrigger);
 
 let sections = gsap.utils.toArray(".panel");
-console.log(sections);
 
 gsap.to(sections, {
     xPercent: -100 * (sections.length - 1),
